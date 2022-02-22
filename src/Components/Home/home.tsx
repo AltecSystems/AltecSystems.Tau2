228e8c78586c70e4b4461c498360b495ec179e47
Sc222 <v.a.shaternikov@gmail.com>
2022-02-22 14:41:40 +0500

    Make design responsive

--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Box, Button, CardContent, Grid, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, CardContent, Divider, Grid, Stack, TextField, Typography } from "@mui/material";
 
 interface HomeProps {}
 
@@ -30,233 +30,247 @@ export class Home extends Component<HomeProps, HomeState> {
     const tau2 = this._calculateTau2(this.state.A, this.state.B, this.state.d, this.state.Ro, this.state.sInputs);
 
     return (
-      <Box sx={{ flexGrow: 1, verticalAlign: "center", p: 4 }}>
-        <Grid container spacing={2} maxWidth="lg">
+      <Box sx={{ flexGrow: 1, verticalAlign: "center", p: 4 }} maxWidth={"md"}>
+        <Stack spacing={0}>
           <CardContent sx={{ width: "100%" }}>
-            <Typography variant="h3" color="text.secondary" gutterBottom sx={{ mb: 5 }}>
+            <Typography variant="h3" color="text.secondary" sx={{ mb: 1 }}>
               Исходные данные
             </Typography>
-
-            <TextField
-              sx={{ maxWidth: "lg", mr: 2 }}
-              label="Коэффициент A"
-              helperText="Габаритная ширина оконного блока"
-              id="A"
-              type="number"
-              defaultValue="0"
-              size="small"
-              value={this.state.A}
-              onChange={(e) => this.setState({ A: Number(e.target.value) })}
-              inputProps={{ step: "0.1" }}
-            />
-
-            <TextField
-              sx={{ maxWidth: "lg", mr: 2 }}
-              label="Коэффициент B"
-              helperText="Габаритная высота оконного блока"
-              id="B"
-              type="number"
-              defaultValue="0"
-              size="small"
-              value={this.state.B}
-              onChange={(e) => this.setState({ B: Number(e.target.value) })}
-              inputProps={{ step: "0.1" }}
-            />
-
-            <TextField
-              sx={{ maxWidth: "lg" }}
-              label="Коэффициент ρ"
-              helperText="Коэфф. Отражения внутренних граней ячеек переплета"
-              id="Ro"
-              type="number"
-              defaultValue="0"
-              size="small"
-              value={this.state.Ro}
-              onChange={(e) => this.setState({ Ro: Number(e.target.value) })}
-              inputProps={{ step: "0.1" }}
-            />
-
-            <br />
-            <TextField
-              sx={{ maxWidth: "lg", mt: 2 }}
-              label="Коэффициент d"
-              helperText="Суммарная толщина переплета, м"
-              id="d"
-              type="number"
-              defaultValue="0"
-              size="small"
-              value={this.state.d}
-              onChange={(e) => this.setState({ d: Number(e.target.value) })}
-              inputProps={{ step: "0.1" }}
-            />
-
-            <Typography variant="h5" color="text.secondary" sx={{ mt: 4 }} gutterBottom>
-              <strong>
+            <Typography variant="h6" color="text.secondary" sx={{ mb: 3 }}>
+              Основные коэффициенты
+            </Typography>
+            <Grid container spacing={2.5} alignContent="flex-start" alignItems="center">
+              <Grid item xs={12} md={6}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Коэффициент A"
+                  helperText="Габаритная ширина оконного блока, м"
+                  id="A"
+                  type="number"
+                  defaultValue="0"
+                  size="small"
+                  value={this.state.A}
+                  onChange={(e) => this.setState({ A: Number(e.target.value) })}
+                  inputProps={{ step: "0.1" }}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Коэффициент B"
+                  helperText="Габаритная высота оконного блока, м"
+                  id="B"
+                  type="number"
+                  defaultValue="0"
+                  size="small"
+                  value={this.state.B}
+                  onChange={(e) => this.setState({ B: Number(e.target.value) })}
+                  inputProps={{ step: "0.1" }}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Коэффициент ρ"
+                  helperText="Коэфф. Отражения внутренних граней ячеек переплета"
+                  id="Ro"
+                  type="number"
+                  defaultValue="0"
+                  size="small"
+                  value={this.state.Ro}
+                  onChange={(e) => this.setState({ Ro: Number(e.target.value) })}
+                  inputProps={{ step: "0.1" }}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} />
+              <Grid item xs={12} md={6}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Коэффициент d"
+                  helperText="Суммарная толщина переплета, м"
+                  id="d"
+                  type="number"
+                  defaultValue="0"
+                  size="small"
+                  value={this.state.d}
+                  onChange={(e) => this.setState({ d: Number(e.target.value) })}
+                  inputProps={{ step: "0.1" }}
+                />
+              </Grid>
+            </Grid>
+            <Typography variant="h6" color="text.secondary" sx={{ mt: 3.5, mb: 1.5 }}>
+              Коэффициенты ячеек
+            </Typography>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
+              <b>
                 a<sub>i</sub>
-              </strong>{" "}
+              </b>{" "}
               - габаритная ширина i-й ячейки в свету, м
             </Typography>
-
-            <Typography variant="h5" color="text.secondary" gutterBottom>
-              <strong>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
+              <b>
                 b<sub>i</sub>
-              </strong>{" "}
+              </b>{" "}
               - габаритная высота i-й ячейки в свету, м
             </Typography>
-
-            <Typography variant="h5" color="text.secondary" gutterBottom>
-              <strong>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
+              <b>
                 σ<sub>i</sub>
-              </strong>{" "}
+              </b>{" "}
               = 2d*(a<sub>i</sub> * b<sub>i</sub>)
             </Typography>
-
-            <Typography variant="h5" color="text.secondary" gutterBottom>
-              <strong>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
+              <b>
                 S<sub>i</sub>
-              </strong>{" "}
+              </b>{" "}
               = a<sub>i</sub> * b<sub>i</sub>
             </Typography>
-
-            <Box>
-              <Stack direction="row" spacing={2} sx={{ pb: 1, pt: 3 }}>
-                <Button
-                  variant="outlined"
-                  onClick={() => {
-                    if (this.state.sInputs.length > 2) {
-                      let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
-                      newState.pop();
-                      this.setState({ sInputs: newState });
-                    }
-                  }}
-                >
-                  -
-                </Button>
-                <Button
-                  variant="outlined"
-                  onClick={() => {
+            <Stack direction="row" spacing={2} sx={{ pb: 0, pt: 4 }}>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  if (this.state.sInputs.length > 2) {
                     let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
-                    newState.push({ a: 0, b: 0 });
+                    newState.pop();
                     this.setState({ sInputs: newState });
-                  }}
-                >
-                  +
-                </Button>
-              </Stack>
-              {this.state.sInputs.map((s, index) => {
-                const indexIncremented = index + 1;
-                return (
-                  <Box sx={{ pt: 3 }}>
-                    <Grid container spacing={6} maxWidth="lg" justifyItems="start" key={indexIncremented} wrap="nowrap">
-                      <Grid item xs={2}>
-                        <TextField
-                          sx={{ width: "100%" }}
-                          label={
-                            <>
-                              a<sub>${indexIncremented}</sub>, м
-                            </>
-                          }
-                          id={`a${indexIncremented}`}
-                          type="number"
-                          defaultValue="0"
-                          size="small"
-                          value={s.a}
-                          onChange={(e) => {
-                            let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
-                            newState[index].a = Number(e.target.value);
-                            this.setState({ sInputs: newState });
-                          }}
-                          inputProps={{ step: "0.1" }}
-                        />
-                      </Grid>
-                      <Grid item xs={2}>
-                        <TextField
-                          sx={{ width: "100%" }}
-                          label={
-                            <>
-                              b<sub>${indexIncremented}</sub>, м
-                            </>
-                          }
-                          id={`b${indexIncremented}`}
-                          type="number"
-                          defaultValue="0"
-                          size="small"
-                          value={s.b}
-                          onChange={(e) => {
-                            let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
-                            newState[index].b = Number(e.target.value);
-                            this.setState({ sInputs: newState });
-                          }}
-                          inputProps={{ step: "0.1" }}
-                        />
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography
-                          variant="h5"
-                          color="text.secondary"
-                          gutterBottom
-                          sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
-                        >
-                          <strong>
-                            σ<sub>{indexIncremented}</sub>
-                          </strong>{" "}
-                          ={this._calculateSigma(this.state.d, s)}м
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography
-                          variant="h5"
-                          color="text.secondary"
-                          gutterBottom
-                          sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
-                        >
-                          <strong>
-                            S<sub>{indexIncremented}</sub>
-                          </strong>{" "}
-                          ={this._calculateSquare(s)}м<sup>2</sup>
-                        </Typography>
-                      </Grid>
+                  }
+                }}
+              >
+                -
+              </Button>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
+                  newState.push({ a: 0, b: 0 });
+                  this.setState({ sInputs: newState });
+                }}
+              >
+                +
+              </Button>
+            </Stack>
+            {this.state.sInputs.map((s, index) => {
+              const indexIncremented = index + 1;
+              return (
+                <Box sx={{ pt: 3 }} key={indexIncremented}>
+                  <Grid container spacing={2} alignContent="flex-start" alignItems="center">
+                    <Grid item xs={6} md={3}>
+                      <TextField
+                        sx={{ width: "100%" }}
+                        label={
+                          <>
+                            a<sub>${indexIncremented}</sub>, м
+                          </>
+                        }
+                        id={`a${indexIncremented}`}
+                        type="number"
+                        defaultValue="0"
+                        size="small"
+                        value={s.a}
+                        onChange={(e) => {
+                          let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
+                          newState[index].a = Number(e.target.value);
+                          this.setState({ sInputs: newState });
+                        }}
+                        inputProps={{ step: "0.1" }}
+                      />
                     </Grid>
-                  </Box>
-                );
-              })}
-            </Box>
+                    <Grid item xs={6} md={3}>
+                      <TextField
+                        sx={{ width: "100%" }}
+                        label={
+                          <>
+                            b<sub>${indexIncremented}</sub>, м
+                          </>
+                        }
+                        id={`b${indexIncremented}`}
+                        type="number"
+                        defaultValue="0"
+                        size="small"
+                        value={s.b}
+                        onChange={(e) => {
+                          let newState = this.state.sInputs.map((inp) => ({ a: inp.a, b: inp.b }));
+                          newState[index].b = Number(e.target.value);
+                          this.setState({ sInputs: newState });
+                        }}
+                        inputProps={{ step: "0.1" }}
+                      />
+                    </Grid>
+                    <Grid item xs={6} md={2}>
+                      <Typography
+                        variant="body1"
+                        color="text.secondary"
+                        gutterBottom
+                        sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
+                      >
+                        <strong>
+                          σ<sub>{indexIncremented}</sub>
+                        </strong>{" "}
+                        ={this._calculateSigma(this.state.d, s)}м
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6} md={2}>
+                      <Typography
+                        variant="body1"
+                        color="text.secondary"
+                        gutterBottom
+                        sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
+                      >
+                        <strong>
+                          S<sub>{indexIncremented}</sub>
+                        </strong>{" "}
+                        ={this._calculateSquare(s)}м<sup>2</sup>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  {index < this.state.sInputs.length - 1 && (
+                    <Divider sx={{ display: { xs: "block", md: "none" }, my: 1 }} />
+                  )}
+                </Box>
+              );
+            })}
           </CardContent>
 
           <CardContent sx={{ width: "100%" }}>
-            <Typography variant="h3" color="text.secondary" gutterBottom sx={{ mb: 5 }}>
+            <Typography variant="h3" color="text.secondary" gutterBottom sx={{ mb: 1 }}>
               Расчет
+            </Typography>
+            <Typography variant="h6" color="text.secondary" gutterBottom sx={{ mb: 3 }}>
+              Итоговые вычисления
             </Typography>
 
             <Typography
-              variant="h5"
+              variant="body1"
               color="text.secondary"
               gutterBottom
-              sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
+              sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: { xs: "unset", md: "wrap" } }}
             >
               <strong>
                 S<sub>0</sub>
               </strong>{" "}
-              - площадь светового проема в снегу = <strong>A * B</strong> ={" "}
+              - площадь светового проема в светы = <strong>A * B</strong> ={" "}
               {this._calculateSquare({ a: this.state.A, b: this.state.B })}м<sup>2</sup>
             </Typography>
-            <br />
             <Typography
-              variant="h5"
+              variant="body1"
               color="text.secondary"
               gutterBottom
-              sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
+              sx={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: { xs: "unset", md: "wrap" } }}
             >
               <strong>
                 τ<sub>2</sub>
               </strong>{" "}
               - коэффициент, учитывающий потери света в переплетах ={" "}
               <strong>
-                {Number.isFinite(tau2) ? tau2 : <span style={{ color: "#E53935" }}>Не все коэффициенты указаны</span>}
+                {Number.isFinite(tau2) ? (
+                  <span style={{ color: "#43A047" }}>{tau2}</span>
+                ) : (
+                  <span style={{ color: "#E53935" }}>Не все коэффициенты указаны</span>
+                )}
               </strong>
             </Typography>
           </CardContent>
-        </Grid>
+        </Stack>
       </Box>
     );
   }
